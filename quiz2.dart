7ca8eca782200b1f4c999ceccd099d500c464eb9
NompieMthombeni <nmbrotty@gmail.com>
2022-06-14 16:54:10 +0200

    ('Question2)

---
+++ quiz2.dart
@@ -0,0 +1,12 @@
+ void main() {
+  List<String> Appname =['takealot,vula,dstv,fnb,pricecheck,RV,Pregnancy Journey,Shyt'];
+  
+  var WinningAPP2017 = 'Shyt';
+  var WinningAPP2018 = 'Besmarta';
+  int Totalnumber =10;
+
+  print(Appname);
+  print(WinningAPP2017);
+  print(WinningAPP2018);
+  print(Totalnumber);
+ }
